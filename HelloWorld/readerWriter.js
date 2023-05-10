b5c9587e48d563e0d55ed2a6491dc48656dc1d0f
Raisul-Islam-Shehab <raisul54@student.sust.edu>
2023-05-10 15:59:10 +0600

    load balance using nginx

---
+++ HelloWorld/readerWriter.js
@@ -0,0 +1,95 @@
+import fs, { access, statSync, constants } from 'fs';
+import * as logger from './logger.js';
+import path from 'path';
+import { pid } from 'process';
+import { ppid } from 'process';
+import process from 'process';
+
+function readFile(req, res, outputFile) {
+
+    //Defines the variable to hold the strings describing the action happening
+    let content = '';
+
+    //Extracts the file name
+    const fileName = req.url;
+
+    //Checks if provided path is valid or not
+    if (fileName == '/') {
+        content = `Didn't provide a valid path\n\n`;
+        logger.appendFile(outputFile, content);
+        res.statusCode = 400;
+        res.end(`Provide a valid path like "localhost:4000/file_name.extension"\n`)
+        return;
+    }
+
+    //Get the IP of the client
+    const parseIp = req.headers['x-forwarded-for']?.split(',').shift()
+        || req.socket?.remoteAddress
+
+    content = `IP is "${parseIp}\n`;
+    logger.appendFile(outputFile, content);
+
+    //Appends the file path to the output file
+    const filePath = "/files" + fileName;
+    content = `Request path extracted "${filePath}"\n`;
+    logger.appendFile(outputFile, content);
+
+    // Checks if a file is accessible or not
+    access(filePath, constants.R_OK | constants.F_OK, (err) => {
+
+        if (err) {
+            if (err.code === 'EACCES') {
+                res.statusCode = 403;
+                content = `File 'Read' permission is denied\n\n`;
+                logger.appendFile(outputFile, content);
+                res.end(content);
+                return;
+            }
+            else {
+                res.statusCode = 404;
+                content = 'File not found\n\n';
+                logger.appendFile(outputFile, content);
+                res.end(content);
+                return;
+            }
+
+        }
+
+        //Get the process id 
+        content = `Process id is "${pid}"\n`;
+        logger.appendFile(outputFile, content)
+
+        //Appends the file extension to the output file
+        let fileExt = path.extname(filePath);
+        content = `File extention is "${fileExt}"\n`;
+        logger.appendFile(outputFile, content);
+
+        //Extracts the file stats and handles errors
+        let stats;
+        try {
+            stats = statSync(filePath);
+        } catch (err) {
+            console.error(err);
+            return;
+        }
+
+        //Appends the file size to the output file
+        content = `File size is "${stats.size}" bytes\n`;
+        logger.appendFile(outputFile, content);
+
+        //Shows the file on browser and appends the action to the output file
+        res.statusCode = 200;
+        content = 'File shown in the browser\n\n';
+        logger.appendFile(outputFile, content);
+
+        try {
+            const stream = fs.createReadStream(filePath);
+            stream.pipe(res);
+        } catch (err) {
+            console.error(err);
+            return;
+        }
+    });
+}
+
+export { readFile };
---
+++ README.md
@@ -0,0 +1,4 @@
+# Load Balance using Nginx
+
+## Use `sudo docker compose up -d` command from terminal to run the application. Make sure you are in the `HelloWorld` directory.
+## Go to `localhost:4000/` from your browser 
